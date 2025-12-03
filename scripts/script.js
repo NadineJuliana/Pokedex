e8d58ab4f0815b9cdc5c5ec9239a3be7e0a67d71
NadineJuliana <nadine.juliana.b@web.de>
2025-12-03 15:15:10 +0100

    add loading function to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const contentRef = document.getElementById('contentRef');
 let pokedexData = [];
 let offset = 0;
-let limit = 31;
+let limit = 30;
 // let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 async function renderTotalContent() {
@@ -10,31 +10,44 @@ async function renderTotalContent() {
 }
 
 async function getData() {
-    for (let i = 1; i < limit; i++) {
+    for (let i = offset + 1; i <= offset + limit; i++) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
         const pokemonData = await response.json();
         pokedexData.push(pokemonData);
         // console.log(pokedexdata);
     }
-
+    offset += limit;
 }
 
 function renderThumbnailRef(){
-    contentRef.innerHTML = "";
-    for (let i = 0; i < pokedexData.length; i++) {
-        
+    
+    for (let i = pokedexData.length - limit; i < pokedexData.length; i++) {
+        const pkm = pokedexData[i];
         contentRef.innerHTML += /*html*/`
-        <div class="thumbnail" id="thumbnailRef${i}">
-            <div>${pokedexData[i].id}</div>
-            <h3>${pokedexData[i].name}</h3>
+        <div onclick="openDialog" class="thumbnail" id="thumbnailRef${i}">
+            <div>${pkm.id}</div>
+            <h3>${pkm.name.toUpperCase()}</h3>
             <div>
-                ${pokedexData[i].types[0].type.name}
-                ${pokedexData[i].types[1]? pokedexData[i].types[1].type.name: ""}
+                ${pkm.types[0].type.name}
+                ${pkm.types[1]? pokedexData[i].types[1].type.name: ""}
             </div>
-            <div><img src="${pokedexData[i].sprites.front_default}"></div>
+            <div><img src="${pkm.sprites.front_default}"></div>
         </div>
     `   
     }
     console.log(pokedexData);
     
 }
+
+async function renderLoadThumbnailRef(){
+    const button = document.getElementById('loadingButton');
+    button.disabled = true;
+    await getData();
+    renderThumbnailRef();
+    setTimeout(() => {
+        button.disabled = false;
+    }, 2000);
+}
+
+
+
