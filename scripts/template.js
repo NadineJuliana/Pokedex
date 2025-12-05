42cd3a04bbe37575f2a67b6c4fc88672dc91ec1b
NadineJuliana <nadine.juliana.b@web.de>
2025-12-05 18:57:56 +0100

    rearranged templates for styling

--- scripts/template.js
+++ scripts/template.js
@@ -1,88 +1,135 @@
 function getThumbnailTemplates(pkm, i) {
-    return `
-        <div onclick="openOverlay(${i}, false)" class="thumbnail bg_${pkm.types[0].type.name}" id="thumbnailRef${i}">
-            <div>#${pkm.id}</div>
-            <div class="thumbnail_Image_Ref"><img class="thumbnail_Image" src="${pkm["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pkm.name}"></div>
-            <h3>${pkm.name.charAt(0).toUpperCase() + pkm.name.slice(1)}</h3>
-            <div class="thumbnail_Types_Ref">
-                <p class="thumbnail_Types bg_${pkm.types[0].type.name}">${pkm.types[0].type.name.charAt(0).toUpperCase() + pkm.types[0].type.name.slice(1)}</p>
-                <p class="thumbnail_Types bg_${pkm.types[1] ? pkm.types[1].type.name : ""}">${pkm.types[1] ? pkm.types[1].type.name.charAt(0).toUpperCase() + pkm.types[1].type.name.slice(1)  : ""}</p>
-            </div> 
+return `
+    <div onclick="openOverlay(${i}, false)" class="thumbnail bg_${pkm.types[0].type.name}" id="thumbnailRef${i}">
+        <div class="pkm_Number">#${pkm.id}</div>
+        <div class="thumbnail_Image_Ref"><img class="thumbnail_Image" src="${pkm["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pkm.name}"></div>
+        <h3>${pkm.name.charAt(0).toUpperCase() + pkm.name.slice(1)}</h3>
+        <div class="thumbnail_Types_Ref">
+            <p class="thumbnail_Types bg_${pkm.types[0].type.name}">${pkm.types[0].type.name.charAt(0).toUpperCase() + pkm.types[0].type.name.slice(1)}</p>
+            <p class="thumbnail_Types bg_${pkm.types[1] ? pkm.types[1].type.name : ""}">${pkm.types[1] ? pkm.types[1].type.name.charAt(0).toUpperCase() + pkm.types[1].type.name.slice(1) : ""}</p>
         </div>
-    `;
+    </div>
+`;
 }
 
-function getFilteredThumbnailTemplates(pkm, i){
-    return `
-        <div onclick="openOverlay(${i}, true)" class="thumbnail bg_${pkm.types[0].type.name}">
-            <div>#${pkm.id}</div>
-            <div class="thumbnail_Image_Ref"><img class="thumbnail_Image" src="${pkm["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pkm.name}"></div>
-            <h3>${pkm.name.charAt(0).toUpperCase() + pkm.name.slice(1)}</h3>
-            <div class="thumbnail_Types_Ref">
-                <p class="thumbnail_Types bg_${pkm.types[0].type.name}">${pkm.types[0].type.name.charAt(0).toUpperCase() + pkm.types[0].type.name.slice(1)}</p>
-                <p class="thumbnail_Types bg_${pkm.types[1] ? pkm.types[1].type.name : ""}">${pkm.types[1] ? pkm.types[1].type.name.charAt(0).toUpperCase() + pkm.types[1].type.name.slice(1) : ""}</p>
-            </div>
+function getFilteredThumbnailTemplates(pkm, i) {
+return `
+    <div onclick="openOverlay(${i}, true)" class="thumbnail bg_${pkm.types[0].type.name}">
+        <div class="pkm_Number">#${pkm.id}</div>
+        <div class="thumbnail_Image_Ref"><img class="thumbnail_Image" src="${pkm["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pkm.name}"></div>
+        <h3>${pkm.name.charAt(0).toUpperCase() + pkm.name.slice(1)}</h3>
+        <div class="thumbnail_Types_Ref">
+            <p class="thumbnail_Types bg_${pkm.types[0].type.name}">${pkm.types[0].type.name.charAt(0).toUpperCase() + pkm.types[0].type.name.slice(1)}</p>
+            <p class="thumbnail_Types bg_${pkm.types[1] ? pkm.types[1].type.name : ""}">${pkm.types[1] ? pkm.types[1].type.name.charAt(0).toUpperCase() + pkm.types[1].type.name.slice(1) : ""}</p>
         </div>
-    `;
+    </div>
+`;
 }
 
-function getOverlayTemplates(pkm, i, isFiltered){
-    return `
-        <div onclick="event.stopPropagation()" class="overlay_Content bg_${pkm.types[0].type.name}" id="overlay">
-            <div class="navigation_icons">
-                <img onclick="openOverlay(${i - 1}, ${isFiltered})" src="assets/icons/001-left-arrow.png" alt="Left Arrow">
-                <img onclick="closeOverlay(${i})" src="assets/icons/001-up-arrow.png" alt="Close">
-                <img onclick="openOverlay(${i + 1}, ${isFiltered})" src="assets/icons/001-right-arrow.png" alt="Right Arrwo">
-            </div>
-            <div class="top_area_overlay">
+function getOverlayTemplates(pkm, i, isFiltered) {
+return `
+    <div onclick="event.stopPropagation()" class="overlay_Content bg_${pkm.types[0].type.name}" id="overlay">
+        <div class="navigation_icons">
+            <img onclick="openOverlay(${i - 1}, ${isFiltered})" src="assets/icons/001-left-arrow.png" alt="Left Arrow">
+            <img onclick="closeOverlay(${i})" src="assets/icons/001-up-arrow.png" alt="Close">
+            <img onclick="openOverlay(${i + 1}, ${isFiltered})" src="assets/icons/001-right-arrow.png" alt="Right Arrwo">
+        </div>
+        <div class="top_area_overlay">
+            <div class="pkm_Infos">
                 <h2>${pkm.name.charAt(0).toUpperCase() + pkm.name.slice(1)}</h2>
-                <p>#${pkm.id}</p>
-                <img src="${pkm.sprites.front_default}" alt="${pkm.name}">
-                <div class="top_types">
-                    <p>${pkm.types[0].type.name}</p>
-                    <p>${pkm.types[1] ? pkm.types[1].type.name : ""}</p>
-                </div>
+                <p class="pkm_Number_Overlay">#${pkm.id}</p>
             </div>
-            <div class="bot_area_overlay">
-                <div class="navigation_stats">
-                    <img src="assets/icons/nav-left.png" alt="Left Arrow">
-                    <h3 id="about${i}">About</h3>
-                    <h3 id="base${i}">Base Stats</h3>
-                    <h3 id="shiny${i}">Shiny</h3>
-                    <img src="assets/icons/nav-right.png" alt="Right Arrow">
-                </div>
-                <div class="stats_ref">
-                    <div id="aboutStats${i}">
-                        <p>Species:</p>
-                        <p>${pkm.species.name}</p>
-                        <p>Height:</p>
-                        <p>${pkm.height}</p>
-                        <p>Weight:</p>
-                        <p>${pkm.weight}</p>
-                        <p>Abilities:</p>
-                        <p>${pkm.abilities[0].ability.name}</p>
-                        <p>${pkm.abilities[1] ? pkm.abilities[1].ability.name : ""}</p>
-                    </div>
-                    <div id="baseStats${i}">
-                        <p>HP</p>
-                        <p>${pkm.stats[0].base_stat}</p>
-                        <p>Attack</p>
-                        <p>${pkm.stats[1].base_stat}</p>
-                        <p>Defense</p>
-                        <p>${pkm.stats[2].base_stat}</p>
-                        <p>Special-Attack</p>
-                        <p>${pkm.stats[3].base_stat}</p>
-                        <p>Special-Defense</p>
-                        <p>${pkm.stats[4].base_stat}</p>
-                        <p>Speed</p>
-                        <p>${pkm.stats[5].base_stat}</p>
-                    </div>
-                    <div id="shinyImage">
-                        <img src="${pkm.sprites.front_shiny}" alt="${pkm.name}">
-                    </div>
-                </div>
+            <div class="top_area_img_Ref">
+                <img src="${pkm["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pkm.name}">
             </div>
-
+            <div class="top_types">
+                <p class="overlay_Types bg_${pkm.types[0].type.name}">${pkm.types[0].type.name.charAt(0).toUpperCase() + pkm.types[0].type.name.slice(1)}</p>
+                <p class="overlay_Types bg_${pkm.types[1] ? pkm.types[1].type.name : ""}">${pkm.types[1] ? pkm.types[1].type.name.charAt(0).toUpperCase() + pkm.types[1].type.name.slice(1) : ""}</p>
+            </div>
         </div>
-    `;
+        <div class="bot_area_overlay">
+            <div class="bot_area_separation">
+                <img src="assets/icons/nav-left.png" alt="Left Arrow">
+                <img src="assets/icons/nav-right.png" alt="Right Arrow">
+            </div>
+            <div class="tabs_container">
+                <div class="tablist" role="tablist" aria-label="Select Tabs">
+                    <button class="tablinks" role="tab" aria-selected="true" aria-controls="firstTabpanel${i}" id="firstTabButton${i}">About</button>
+                    <button class="tablinks"  role="tab" aria-selected="false" aria-controls="secondTabpanel${i}" id="secondTabButton${i}">Base Stats</button>
+                    <button class="tablinks"  role="tab" aria-selected="false" aria-controls="thirdTabpanel${i}" id="thirdTabButton${i}">Appearances</button>
+                </div>  
+            </div>
+            <div class="about_Stats" id="firstTabpanel${i}" role="tabpanel" aria-labelledby="firstTabButton${i}">
+                <div class="about">
+                    <h4>Species:</h4>
+                    <p>${pkm.species.name}</p>
+                </div>
+                <div class="about">
+                    <h4>Height:</h4>
+                    <p>${pkm.height}</p>
+                </div>
+                <div class="about">
+                    <h4>Weight:</h4>
+                    <p>${pkm.weight}</p>
+                </div>
+                <div class="about">
+                    <h4>Abilities:</h4>
+                    <p>${pkm.abilities[0].ability.name}</p>
+                </div>
+                <div class="about">
+                    <p><p>
+                    <p>${pkm.abilities[1] ? pkm.abilities[1].ability.name : ""}</p>
+                </div>
+            </div>
+            <div class="base_Stats" id="secondTabpanel${i}" role="tabpanel" aria-labelledby="secondTabButton${i}" hidden>
+                <div class="stats">
+                    <p>HP</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[0].base_stat, 100)}%">${pkm.stats[0].base_stat}</div>
+                    </div>
+                </div>
+                <div class="stats">
+                    <p>Attack</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[1].base_stat, 100)}%">${pkm.stats[1].base_stat}</div>
+                    </div>
+                </div>
+                <div class="stats">
+                    <p>Defense</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[2].base_stat, 100)}%">${pkm.stats[2].base_stat}</div>
+                    </div>
+                </div>
+                <div class="stats">
+                    <p>Special-Attack</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[3].base_stat, 100)}%">${pkm.stats[3].base_stat}</div>
+                    </div>
+                </div>
+                <div class="stats">
+                    <p>Special-Defense</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[4].base_stat, 100)}%">${pkm.stats[4].base_stat}</div>
+                    </div>
+                </div>
+                <div class="stats">
+                    <p>Speed</p>
+                    <div class="progressbar_Ref">
+                        <div class="progressbar" style="width: ${Math.min(pkm.stats[5].base_stat, 100)}%">${pkm.stats[5].base_stat}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="appearances" id="thirdTabpanel${i}" role="tabpanel" aria-labelledby="thirdTabButton${i}" hidden>
+                <div class="appearance">
+                    <h4>Normal Appearance</h4>
+                    <img src="${pkm.sprites.front_default}" alt="${pkm.name}">
+                </div>
+                <div class="appearance">
+                    <h4>Shiny Appearance</h4>
+                   <img src="${pkm.sprites.front_shiny}" alt="${pkm.name}">     
+                </div> 
+            </div>
+        </div>
+    </div>
+`;
 }
